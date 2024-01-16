da70117353766a5608d71fe7530c45813cd4c5d9
rapheallzz <70053913+rapheallzz@users.noreply.github.com>
2024-01-16 21:19:35 +0100

    Update multer.js

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,31 +1,17 @@
 const multer = require('multer');
-const multerS3 = require('multer-s3');
-const aws = require('aws-sdk');
+const path = require('path');
 
-aws.config.update({
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  region: process.env.AWS_REGION,
-});
+// multer config
 
-const s3 = new aws.S3();
-
-const upload = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: 'your-s3-bucket-name',
-    key: function (req, file, cb) {
-      cb(null, 'path/in/s3/' + Date.now() + '-' + file.originalname);
-    },
-  }),
-  fileFilter: (req, file, cb) => {
-    let ext = path.extname(file.originalname);
-    if (ext !== '.jpg' && ext !== '.jpeg' && ext !== '.png' && ext !== '.mp4') {
-      cb(new Error('File type is not supported'), false);
-      return;
+module.exports = multer({
+    storage: multer.diskStorage({}),
+    fileFilter: (req, file, cb) => {
+        let ext = path.extname(file.originalname);
+        if(ext !==".jpg" && ext !==".jpeg" && ext !==".png" && ext !==".mp4") {
+            cb(new Error ("File type is not supported"), false);
+            return;
+        }
+        cb(null, true)
     }
-    cb(null, true);
-  },
-});
+}) // Add this line
 
-module.exports = upload;
