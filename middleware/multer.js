733ae3dc2e450126062f7226d6745170eb8e93c6
rapheallzz <70053913+rapheallzz@users.noreply.github.com>
2024-01-16 17:22:41 +0100

    Update multer.js

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,36 +1,20 @@
-// multer config
 const multer = require('multer');
 const path = require('path');
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/'); // Set the destination folder for file uploads
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname)); // Rename file with timestamp
-  },
-});
+const storage = multer.diskStorage({});
 
 const fileFilter = (req, file, cb) => {
-  const allowedFileTypes = ['.jpg', '.jpeg', '.png', '.mp4'];
-
-  const extname = path.extname(file.originalname).toLowerCase();
-  if (!allowedFileTypes.includes(extname)) {
+  const ext = path.extname(file.originalname);
+  if (ext !== '.jpg' && ext !== '.jpeg' && ext !== '.png' && ext !== '.mp4') {
     cb(new Error('File type is not supported'), false);
     return;
   }
-
   cb(null, true);
 };
 
-module.exports = multer({
+const upload = multer({
   storage: storage,
   fileFilter: fileFilter,
-}).fields([
-  { name: 'video', maxCount: 1 }, // Field for video file
-  { name: 'image', maxCount: 1 }, // Field for image file
-]);
+});
 
-
-
-
+module.exports = upload;
