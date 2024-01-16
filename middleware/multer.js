fd749c5632191d51742244ccb84e4ab3ab7cdf15
rapheallzz <70053913+rapheallzz@users.noreply.github.com>
2024-01-16 17:10:13 +0100

    Update multer.js
    multer update

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,19 +1,35 @@
+// multer config
 const multer = require('multer');
 const path = require('path');
 
-// multer config
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // Set the destination folder for file uploads
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname)); // Rename file with timestamp
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  const allowedFileTypes = ['.jpg', '.jpeg', '.png', '.mp4'];
+
+  const extname = path.extname(file.originalname).toLowerCase();
+  if (!allowedFileTypes.includes(extname)) {
+    cb(new Error('File type is not supported'), false);
+    return;
+  }
+
+  cb(null, true);
+};
 
 module.exports = multer({
-    storage: multer.diskStorage({}),
-    fileFilter: (req, file, cb) => {
-        let ext = path.extname(file.originalname);
-        if(ext !==".jpg" && ext !==".jpeg" && ext !==".png" && ext !==".mp4") {
-            cb(new Error ("File type is not supported"), false);
-            return;
-        }
-        cb(null, true)
-    }
-}) // Add this line
+  storage: storage,
+  fileFilter: fileFilter,
+}).fields([
+  { name: 'video', maxCount: 1 }, // Field for video file
+  { name: 'image', maxCount: 1 }, // Field for image file
+]);
 
 
 
