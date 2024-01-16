9382b170bbcee210e47c5c3a6329bc7df340a2cb
rapheallzz <70053913+rapheallzz@users.noreply.github.com>
2024-01-16 17:19:15 +0100

    Update contentRoute.js
    updated content route

--- Routes/contentRoute.js
+++ Routes/contentRoute.js
@@ -1,18 +1,12 @@
 const express = require('express');
-const router = express.Router()
-const {getContent,postLikes, unlike, createContent,updateContent, deleteContent} = require('../controllers/contentController');
-  const upload = require('../middleware/multer')
-  // const { single: uploadSingle } = require('../middleware/contentMulter');
-  
+const router = express.Router();
+const { getContent, postLikes, unlike, createContent, updateContent, deleteContent } = require('../controllers/contentController');
+const upload = require('../middleware/multer');
 
-// router.route('/').get(getContent).post(uploadSingle('video'), createContent);
-router.route('/').get(getContent).post( upload.single("video"), createContent);
-router.route('/:id').put(upload.single("video"), updateContent).delete(deleteContent); 
+router.route('/').get(getContent).post(upload.fields([{ name: 'video', maxCount: 1 }, { name: 'image', maxCount: 1 }]), createContent);
+router.route('/:id').put(upload.single('video'), updateContent).delete(deleteContent);
 
 router.route('/like').put(postLikes);
-router.route('/unlike').put(unlike)
-  // router.route('/:id').put(updateTop10).delete(deleteTop10) 
+router.route('/unlike').put(unlike);
 
-
-
-module.exports = router  
+module.exports = router;
