560a3363e9c31d2339a1653ded0973e515845a31
rapheallzz <70053913+rapheallzz@users.noreply.github.com>
2024-01-16 20:58:18 +0100

    Update multer.js

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,17 +1,26 @@
 const multer = require('multer');
 const path = require('path');
 
-// multer config
+// Set the destination directory for file storage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'path/to/your/upload/directory');
+  },
+  filename: function (req, file, cb) {
+    // You can customize the filename if needed
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  },
+});
 
+// Multer config
 module.exports = multer({
-    storage: multer.diskStorage({}),
-    fileFilter: (req, file, cb) => {
-        let ext = path.extname(file.originalname);
-        if(ext !==".jpg" && ext !==".jpeg" && ext !==".png" && ext !==".mp4") {
-            cb(new Error ("File type is not supported"), false);
-            return;
-        }
-        cb(null, true)
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    let ext = path.extname(file.originalname);
+    if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png" && ext !== ".mp4") {
+      cb(new Error("File type is not supported"), false);
+      return;
     }
-}) // Add this line
-
+    cb(null, true);
+  },
+});
