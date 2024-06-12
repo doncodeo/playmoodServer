e39415c9fc40fb7ea681eae4b5306563ddbfed76
ITA PC <doncodeo@gmail.com>
2024-06-12 19:11:47 +0100

    update server

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // MongoDB Configuration 
 const uri = process.env.MONGO_CONNECTION_STRING;
@@ -39,9 +39,6 @@ connectDB()
     app.use('/api/content', require('./Routes/contentRoute'));
     app.use('/api/user', require('./Routes/userRoute'));
 
-      // Handling Preflight OPTIONS requests
-      app.options('*', cors());
-
     // Serve login.html for /login route
     app.get('/login', (req, res) => {
       res.sendFile(path.join(__dirname, 'public', 'login.html'));
@@ -54,12 +51,8 @@ connectDB()
       }
     });
 
-    // // Handling Preflight OPTIONS requests
-    // app.options('*', cors());
-
     // Start the server 
     app.listen(port, () => console.log(`Server started on port ${port}`));
-
   })
   .catch((error) => {
     console.error(error);
@@ -68,11 +61,6 @@ connectDB()
 
 
 
-
-
-
-
-  
 
 
 // const express = require('express');
@@ -112,12 +100,12 @@ connectDB()
 //     app.use(bodyParser.json());
 //     app.use(bodyParser.urlencoded({ extended: true }));
 
-//     // Handling Preflight OPTIONS requests
-//     app.options('*', cors());
-
 //     // Routes 
 //     app.use('/api/content', require('./Routes/contentRoute'));
 //     app.use('/api/user', require('./Routes/userRoute'));
+
+//       // Handling Preflight OPTIONS requests
+//       app.options('*', cors());
 
 //     // Serve login.html for /login route
 //     app.get('/login', (req, res) => {
@@ -131,6 +119,9 @@ connectDB()
 //       }
 //     });
 
+//     // // Handling Preflight OPTIONS requests
+//     // app.options('*', cors());
+
 //     // Start the server 
 //     app.listen(port, () => console.log(`Server started on port ${port}`));
 
@@ -139,4 +130,77 @@ connectDB()
 //     console.error(error);
 //     process.exit(1);
 //   });
+
+
+
+
+
+
+  
+
+
+// // const express = require('express');
+// // const cors = require('cors');
+// // const bodyParser = require('body-parser');
+// // const dotenv = require('dotenv').config();
+// // const connectDB = require('./config/db');
+// // const { MongoClient } = require('mongodb');
+// // const path = require('path');
+
+// // const app = express();
+// // const port = process.env.PORT || 5000;
+
+// // app.use(express.static('public'));
+
+// // // MongoDB Configuration 
+// // const uri = process.env.MONGO_CONNECTION_STRING;
+// // const client = new MongoClient(uri);
+
+// // // Connect to MongoDB 
+// // connectDB()
+// //   .then(() => {
+// //     // Enable CORS 
+// //     const allowedOrigins = [
+// //       'http://localhost:5173',
+// //       'https://playmoodtv.com',
+// //       // Add any other origins as needed
+// //     ];
+
+// //     app.use(cors({
+// //       origin: allowedOrigins,
+// //     }));
+
+// //     // Middleware for JSON and URL-encoded data
+// //     app.use(express.json());
+// //     app.use(express.urlencoded({ extended: false }));
+// //     app.use(bodyParser.json());
+// //     app.use(bodyParser.urlencoded({ extended: true }));
+
+// //     // Handling Preflight OPTIONS requests
+// //     app.options('*', cors());
+
+// //     // Routes 
+// //     app.use('/api/content', require('./Routes/contentRoute'));
+// //     app.use('/api/user', require('./Routes/userRoute'));
+
+// //     // Serve login.html for /login route
+// //     app.get('/login', (req, res) => {
+// //       res.sendFile(path.join(__dirname, 'public', 'login.html'));
+// //     });
+
+// //     // Serve index.html for any other route (excluding OPTIONS method)
+// //     app.get('*', (req, res) => {
+// //       if (req.method !== 'OPTIONS') {
+// //         res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// //       }
+// //     });
+
+// //     // Start the server 
+// //     app.listen(port, () => console.log(`Server started on port ${port}`));
+
+// //   })
+// //   .catch((error) => {
+// //     console.error(error);
+// //     process.exit(1);
+// //   });
 
