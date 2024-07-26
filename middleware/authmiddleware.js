8fe91454a9d523b15bc5875b329da0bb08cd078a
rapheallzz <70053913+rapheallzz@users.noreply.github.com>
2024-07-26 07:16:45 +0100

    Update authmiddleware.js
    included a user decoding script            
    
     if (!req.user) {
                    res.status(401);
                    throw new Error('User not found');
                }

--- middleware/authmiddleware.js
+++ middleware/authmiddleware.js
@@ -11,19 +11,21 @@ const protect = asyncHandler(async (req, res, next) => {
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         try {
             token = req.headers.authorization.split(' ')[1];
-
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
             req.user = await User.findById(decoded.id).select('-password');
+
+            if (!req.user) {
+                res.status(401);
+                throw new Error('User not found');
+            }
 
             next();
         } catch (error) {
             res.status(401);
             throw new Error('Not authorized, token failed');
         }
-    }
-
-    if (!token) {
+    } else {
         res.status(401);
         throw new Error('Not authorized, no token');
     }
