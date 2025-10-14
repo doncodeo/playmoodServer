84a3c0c77cba65957597811ff9e2b4ae646eb4f0
Emmanuel Omolewu <sortplux@gmail.com>
2025-10-14 08:01:05 +0100

    updated the general rate limit from 200 - 300

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -10,7 +10,7 @@ const loginLimiter = rateLimit({
 
 const generalLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 300, // limit each IP to 100 requests per windowMs
   message: 'Too many requests from this IP, please try again after 15 minutes',
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
